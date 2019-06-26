4ce375de335cb963d4de6c5b9ec222b97b7721bd
Patrick Weigelt <weigelt.patrick@gmail.com>
2019-06-26 15:08:27 +0200

    Create extract_raster.R

---
+++ extract_raster.R
@@ -0,0 +1,9 @@
+# alternative approach using rasterize()
+
+extract_aggr <- function(polygon, raster) { 
+    ext = extent(polygon) 
+    raster_crop = crop(raster, ext)
+    polygon_raster = rasterize(polygon, raster_crop, field = 1) # Rasterize Polygons
+    polygon_values = getValues(raster_crop)[which(polygon_raster@data@values == 1)]
+    return(mean(polygon_values,na.rm=TRUE))
+}

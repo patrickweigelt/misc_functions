be22a8b405bc4b299d486d43eba11b85138a815b
Patrick Weigelt <weigelt.patrick@gmail.com>
2020-02-19 14:45:25 +0100

    Update extract_raster.R

--- extract_raster.R
+++ extract_raster.R
@@ -3,7 +3,7 @@
 extract_aggr <- function(polygon, raster) { 
     ext = extent(polygon) 
     raster_crop = crop(raster, ext)
-    polygon_raster = rasterize(polygon, raster_crop, field = 1) # Rasterize Polygons
+    polygon_raster = rasterize(polygon, raster_crop, field = 1) # Rasterize Polygons (check: # fasterize::fasterize)
     polygon_values = getValues(raster_crop)[which(polygon_raster@data@values == 1)]
     return(mean(polygon_values,na.rm=TRUE))
 }
